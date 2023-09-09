0c05edc94d3d3d74668dec302eb555a315ad6b8f
CaptMousse <sefvdx@me.com>
2023-09-09 10:15:27 +0800

    前端支持名片弹出框

--- WebRoot/resource/js/baseFunction.js
+++ WebRoot/resource/js/baseFunction.js
@@ -64,7 +64,7 @@ function ajax(method, controller, async, formData) {
 
 function getUuid() {
     var temp_url = URL.createObjectURL(new Blob());
-    var uuid = temp_url.toString(); // blob:https://xxx.com/b250d159-e1b6-4a87-9002-885d90033be3
+    var uuid = temp_url.toString();
     URL.revokeObjectURL(temp_url);
     return uuid.substr(uuid.lastIndexOf("/") + 1);
 }
